64b39024f6ced2ce4ed9770447bbe99115cd94c1
240-fps <rjmao240fps@gmail.com>
2025-01-21 03:02:02 +0000

    e

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Page Navigation
 const pages = document.querySelectorAll('.page');
 let currentPage = 0;
 
@@ -11,25 +10,30 @@ function goToPage(index) {
   }
 }
 
+function updateDots() {
+  const dots = document.querySelectorAll('.navigation-dots .dot');
+  dots.forEach((dot, index) => {
+    dot.classList.toggle('active', index === currentPage);
+  });
+}
+
 document.getElementById('yes-btn-1').addEventListener('click', () => goToPage(1));
 document.getElementById('yes-btn-2').addEventListener('click', () => goToPage(1));
 
 // Swipe Navigation
 let startX = 0;
+
 document.addEventListener('touchstart', (e) => {
   startX = e.touches[0].clientX;
 });
 
 document.addEventListener('touchend', (e) => {
   const endX = e.changedTouches[0].clientX;
-  if (startX - endX > 50) {
-    goToPage(currentPage + 1);
-  } else if (startX - endX < -50) {
-    goToPage(currentPage - 1);
-  }
+  if (startX - endX > 50) goToPage(currentPage + 1);
+  if (startX - endX < -50) goToPage(currentPage - 1);
 });
 
-// Itinerary Logic
+// Itinerary Cards
 function toggleCard(selectedCard) {
   const cards = document.querySelectorAll('.itinerary-card');
   cards.forEach((card) => {
